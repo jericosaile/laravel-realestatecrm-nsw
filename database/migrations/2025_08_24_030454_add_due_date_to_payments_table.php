971100c5b1c9b587b6ce9c01d023bfe840640657
jericosaile <sailejerico8@gmail.com>
2025-08-24 16:36:14 +0800

    Removed the Duplicate

--- database/migrations/2025_08_24_030454_add_due_date_to_payments_table.php
+++ database/migrations/2025_08_24_030454_add_due_date_to_payments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('payments', function (Blueprint $table) {
             $table->date('due_date')->nullable();
-            $table->string('status')->default('pending');
 
         });
     }
